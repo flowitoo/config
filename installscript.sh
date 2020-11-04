569d10ae5221595579870a3d1ff28eb0423ca831
elf <61194758+e1fy@users.noreply.github.com>
2020-11-04 08:51:23 +0000

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -1,4 +1,5 @@
 # Downloading everything needed
+sudo pacman -Syu
 sudo pacman -S i3-wm i3-gaps i3lock dmenu network-manager-applet acpi neofetch lolcat i3status nitrogen pavucontrol git
 git clone https://github.com/e1fy/config/
 # Starts to work

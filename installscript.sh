4e78f4cfb799cf569a72866db4e6b628ba9cbdd5
elf <61194758+e1fy@users.noreply.github.com>
2020-11-04 08:46:25 +0000

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -5,6 +5,9 @@ git clone https://github.com/e1fy/config/
 cd config
 convert -resize $(xdpyinfo | grep dimensions | cut -d\  -f7 | cut -dx -f1) 1.png fix1.png
 convert -resize $(xdpyinfo | grep dimensions | cut -d\  -f7 | cut -dx -f1) 2.png fix2.png
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir /home/$USER/Documents
+fi
 mkdir /home/$USER/Documents/pics
 mv 1.png /home/$USER/Documents/pics;mv 2.png /home/$USER/Documents/pics
 mv i3cfg /home/$USER/.config/i3/config
